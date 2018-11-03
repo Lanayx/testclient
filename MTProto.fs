061eab68511eeedc0e1d1a28121c959ab5ad793c
Vladimir Shchur <shchurvladimir@gmail.com>
2018-11-03 17:49:19 +0300

    Added serialization / deserialization

---
+++ MTProto.fs
@@ -0,0 +1,34 @@
+﻿module MTProto
+
+
+open System.Buffers
+open System
+open System
+open Microsoft.FSharp
+
+
+type UnencryptedMessage =
+    {
+        AuthId: int64
+        MsgId: int64
+        Bytes: byte[]
+    }
+
+let encode (msg: UnencryptedMessage) =
+    let resultBuffer = Array.zeroCreate<byte>(24 + msg.Bytes.Length)
+    let bufferSpan = resultBuffer.AsSpan()
+    BitConverter.TryWriteBytes(bufferSpan.Slice(0, 8), msg.AuthId) |> ignore
+    BitConverter.TryWriteBytes(bufferSpan.Slice(8, 8), msg.MsgId) |> ignore
+    BitConverter.TryWriteBytes(bufferSpan.Slice(16, 4), msg.Bytes.Length) |> ignore
+    BitConverter.TryWriteBytes(bufferSpan.Slice(20, 4), 0x60469778) |> ignore
+    Buffer.BlockCopy(msg.Bytes, 0, resultBuffer, 24, msg.Bytes.Length)
+    resultBuffer
+
+let decode (msg: byte[]) =
+    let authId = BitConverter.ToInt64(msg, 0)
+    let msgId = BitConverter.ToInt64(msg, 8)
+    let bytesLength = BitConverter.ToInt32(msg, 16)
+    let bytes = Array.zeroCreate<byte>(bytesLength)
+    Buffer.BlockCopy(msg, 24, bytes, 0, bytes.Length)
+    { AuthId = authId; MsgId = msgId; Bytes = bytes }
+
--- Program.fs
+++ Program.fs
@@ -5,34 +5,33 @@ open Pipelines.Sockets.Unofficial
 open FSharp.Control.Tasks.V2.ContextInsensitive
 open System.Buffers
 
+open MTProto
+
 let port = 3000
 let address = "127.0.0.1"
-let hexStringToByteArray (hex : string) =
-    [| 0 .. hex.Length-1 |]
-    |> Array.filter (fun x -> x % 2 = 0)
-    |> Array.map (fun x -> Convert.ToByte(hex.Substring(x, 2), 16))
+
+
+let rand = Random()
+let timestamp() =
+    (DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, 0)).TotalSeconds |> int64;
 
 let doWork() =
     printfn "Starting..."
 
     let endpoint = IPEndPoint(IPAddress.Parse(address), port)
-    let encoding = UTF32Encoding(false, false, false);
-
-    // should work from https://stackoverflow.com/a/41663225/1780648
+    let nonceBuffer = Array.zeroCreate<byte>(16);
+    rand.NextBytes(nonceBuffer)
+    let timestamp = timestamp()
     let data =
-        "EF0A"+
-        "0000000000000000"+
-        "00203DC050937B58"+
-        "14000000"+
-        "78974660"+
-        "00BB27062BF84D9BBE9C7BB192559FE5"
-        |> hexStringToByteArray
-        |> ReadOnlyMemory<byte>
-
+        MTProto.encode { AuthId = 0L; MsgId = timestamp; Bytes = nonceBuffer }
+    let test =
+        MTProto.decode data
+    printfn "Encode decode success=%b" (test.MsgId = timestamp)
+    printfn "Sending data [%s]" (BitConverter.ToString(data))
     task {
         try
             use! conn = SocketConnection.ConnectAsync(endpoint)
-            let! flushResult = conn.Output.WriteAsync(data)
+            let! flushResult = conn.Output.WriteAsync(data |> ReadOnlyMemory)
             conn.Output.Complete()
             let mutable continueLooping = true
             while continueLooping do
@@ -42,8 +41,8 @@ let doWork() =
                 if result.IsCompleted
                 then
                     let array = result.Buffer.ToArray()
-                    let received = encoding.GetString(array)
-                    printfn "Message received: %s" received
+                    let received = decode array
+                    printfn "Message received: %A" received
                     continueLooping <- false
                 else
                     printfn "Waiting..."
@@ -54,8 +53,6 @@ let doWork() =
 
 [<EntryPoint>]
 let main argv =
-
     doWork().Wait()
-
     0
 
